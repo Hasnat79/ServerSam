8c594ebcc96189d12502054c5dd3ad12acdafb63
Adyel <adyel@live.com>
2018-04-28 14:39:38 +0600

    fxml path fixed

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,12 +11,11 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-        DBConnect.createNewDatabase();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main/UserInterface.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("UserInterface.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
